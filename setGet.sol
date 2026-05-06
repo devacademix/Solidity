43bf6ced5e52521abf84d1bfd1de5385bc584767
vishal Paswan <vishalgkp222@gmail.com>
2026-05-06 12:59:06 +0530

    function setter and getter

---
+++ setGet.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract local
+{
+    uint age = 18;
+
+    function getter() public view returns (uint)
+    {
+        return age;
+    }
+    function setter() public
+    {
+        age=age+1;
+    }
+}
